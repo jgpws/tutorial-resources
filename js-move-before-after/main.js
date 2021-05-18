3a41f4909d492798cafae37fc7def3747421a3dc
Jason Gonzalez <jason.sgonzalez@gmail.com>
2021-05-17 18:58:58 -0700

    Add files via upload

---
+++ js-move-before-after/main.js
@@ -0,0 +1,17 @@
+// Global Variables
+
+
+// Counter from MDN Closures article; https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
+
+
+function moveImgRight() {
+
+}
+
+
+function moveImgLeft() {
+
+}
+
+
+// Event Listeners
